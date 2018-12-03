25ccea853909d3b0d3d5f39ac367228327e7d702
Evanaellio <evan.mottais@gmail.com>
2018-12-03 14:32:28 +0100

    Add sample json data for chained questions

--- src/data/quizz.js
+++ src/data/quizz.js
@@ -1,6 +1,6 @@
 var INTRO = {
     title: "Bienvenue sur le quizz !"
-}
+};
 
 var QUIZZ_EASY = [
     {
@@ -13,10 +13,10 @@ var QUIZZ_EASY = [
     {
         type:"default",
         title:"Evenement connu à Rennes cette semaine ?",
-        choices: ["marathon","transmusicale","reponseD"],
+        choices: ["marathon","transmusicale","reponseD", "onemore"],
         answer: 1
     }
-]
+];
 
 var QUIZZ_NORMAL = [
     {
@@ -32,7 +32,27 @@ var QUIZZ_NORMAL = [
         step:1,
         answer:3
     },
-]
+    {
+        type: "chained",
+        questions: [
+            {
+                title: "Nouveau moyen de transport sur Rennes ?",
+                choices: ["vélo", "train", "navette", "bus"],
+                answer: 1
+            },
+            {
+                title: "A quel endroit ?",
+                choices: ["campus beaulieu", "centre-ville", ""],
+                answer: 1
+            },
+            {
+                title: "Particularité ?",
+                choices: ["fonctionne au gaz", "autonome"],
+                answer: 1
+            },
+        ]
+    }
+];
 
 var GAME = {
     intro: INTRO,
@@ -46,4 +66,5 @@ var GAME = {
             quizz: QUIZZ_NORMAL,
         },
     ]
-}
+};
+
