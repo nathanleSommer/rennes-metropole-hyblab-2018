2489e858fed88d272af5fff2358a08a6122ad17e
Unknown <lesommer.nathan@gmail.com>
2018-12-03 15:10:21 +0100

    Adding answer display container and sliding-in animation.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,6 +26,16 @@ Handlebars.registerHelper({
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+ 
+  //Affiche le panneau de réponse au moment où l'on clique sur une proposition
+  document.getElementById('answer--btn').addEventListener('click', function(){
+    document.getElementById('answer--wrapper').classList.toggle('close');
+  });
+
+  document.getElementById('answer--wrapper').addEventListener('click', function(){
+    document.getElementById('answer--wrapper').classList.toggle('close');
+  });
+
   var lastElementClicked;
   var PrevLink = document.querySelector('a.prev');
   var NextLink = document.querySelector('a.next');
