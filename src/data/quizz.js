41e73cea2a7c7887f0f6c8a60b5c6e8b6ef4181b
Unknown <lesommer.nathan@gmail.com>
2018-12-04 16:40:01 +0100

    Updating quizz

--- src/data/quizz.js
+++ src/data/quizz.js
@@ -12,45 +12,36 @@ var OUTRO = {
 
 var QUIZZ_EASY = [
     {
-        type: "map",
-        title: "Pouvez-vous retracer le chemin de la ligne B du métro ?"
-    },
-    {
         type:"cursor",
-        title:"Quel est l'âge moyen des rennais ?",
-        range:[20,100],
-        step:5,
-        theme:"immeuble",
-        answer:35
-    },
-    {
-        type:"default",
-        title:"Evenement connu à Rennes cette semaine ?",
-        choices: ["marathon","transmusicale","reponseD", "onemore"],
+        title:"L'association \"La Belle Déchette\" collecte actuellement 86 tonnes d'objets qui seront recyclés plus tard. Combien de tonnes cherche-t-elle à collecter d'ici 2020 ?",
+        range : [100,300],
+        step: 10,
         theme : "poubelle",
-        answer: 1
+        answer: 270,
+        text: "En effet, plus de 3 fois la quantité actuelle de déchets seront récoltés et recyclés d'ici 2020 !"
     },
-];
-
-var QUIZZ_NORMAL = [
     {
         type:"default",
-        title:"Evenement connu à Rennes cette semaine ?",
-        choices: ["marathon","transmusicale","reponseD"],
-        theme : "transport",
-        answer: 1
-    },
-    {
-        type:"cursor",
-        title:"Combien de Kg d'ordures ménagères jette un usager par an?",
+        title:"La ville de Rennes cherche à renforcer son plan d'action pour la propreté. COmbien de poubelles de rue sont actuellement à la disposition des rennais ?",
         theme:"poubelle",
-        range:[0,500],
-        step:50,
-        answer:192
+        choices: ["700","1000","1300", "1600"],
+        answer:2,
+        text: "L'objectif de la ville de Rennes est de doubler ce nombre d'ici 2020."
     },
+
+    {
+        type:"default",
+        title:"De lourds travaux seront lancés en 2022 à l'usine d'incinération de Villejean. Combien de tonnes de déchets sont-elles brûlées annuellement ?",
+        theme:"poubelle",
+        choices: ["50 000 tonnes","90 000 tonnes","120 000 tonnes", "140 000 tonnes"],
+        answer:3,
+        text: "Cela représente à peu près 380 tonnes de déchets par jours !"
+    },
+
+
     {
         type:"cursor",
-        title:"De combien d'étage dispose la nouvelle tour Samsic?",
+        title:"L'entreprise Samsic installe son siège près de la gare, où une tour de 100 mètres sera construite. Combien d'étages cela représente-t-il ?",
         theme:"immeuble",
         range:[8,32],
         step:3,
@@ -62,22 +53,94 @@ var QUIZZ_NORMAL = [
         theme: "transport",
         questions: [
             {
-                title: "Nouveau moyen de transport sur Rennes ?",
+                title: "Un nouveau moyen de transport est actuellement en phase de test à Rennes, de quoi s'agit t'il ?",
                 choices: ["vélo", "train", "navette", "bus"],
-                answer: 1
+                answer: 2
             },
             {
-                title: "A quel endroit ?",
-                choices: ["campus beaulieu", "centre-ville", ""],
-                answer: 1
+                title: "Où cette navette se trouve t-elle ? ?",
+                choices: ["Sur le campus de Beaulieu", "Dans le centre-ville"],
+                answer: 0
             },
             {
-                title: "Particularité ?",
-                choices: ["fonctionne au gaz", "autonome"],
+                title: "Quelle est la particularité de cette navette",
+                choices: ["elle fonctionne au gaz", "elle est entièrement autonome"],
                 answer: 1
             },
         ]
+    },
+    {
+        type: "map",
+        theme:"transport",
+        title: "Seriez-vous capable de relier les points qui forment la nouvelle ligne B du métro de Rennes ?"
     }
+];
+
+var QUIZZ_NORMAL = [
+    {
+        type:"cursor",
+        title:"L'association \"La Belle Déchette\" collecte actuellement 86 tonnes d'objets qui seront recyclés plus tard. Combien de tonnes cherche-t-elle à collecter d'ici 2020 ?",
+        range : [100,300],
+        step: 10,
+        theme : "poubelle",
+        answer: 270,
+        text: "En effet, plus de 3 fois la quantité actuelle de déchets seront récoltés et recyclés d'ici 2020 !"
+    },
+    {
+        type:"default",
+        title:"La ville de Rennes cherche à renforcer son plan d'action pour la propreté. COmbien de poubelles de rue sont actuellement à la disposition des rennais ?",
+        theme:"poubelle",
+        choices: ["700","1000","1300", "1600"],
+        answer:2,
+        text: "L'objectif de la ville de Rennes est de doubler ce nombre d'ici 2020."
+    },
+
+    {
+        type:"default",
+        title:"De lourds travaux seront lancés en 2022 à l'usine d'incinération de Villejean. Combien de tonnes de déchets sont-elles brûlées annuellement ?",
+        theme:"poubelle",
+        choices: ["50 000 tonnes","90 000 tonnes","120 000 tonnes", "140 000 tonnes"],
+        answer:3,
+        text: "Cela représente à peu près 380 tonnes de déchets par jours !"
+    },
+
+
+    {
+        type:"cursor",
+        title:"L'entreprise Samsic installe son siège près de la gare, où une tour de 100 mètres sera construite. Combien d'étages cela représente-t-il ?",
+        theme:"immeuble",
+        range:[8,32],
+        step:3,
+        answer:26
+    },
+
+    {
+        type: "chained",
+        theme: "transport",
+        questions: [
+            {
+                title: "Un nouveau moyen de transport est actuellement en phase de test à Rennes, de quoi s'agit t'il ?",
+                choices: ["vélo", "train", "navette", "bus"],
+                answer: 2
+            },
+            {
+                title: "Où cette navette se trouve t-elle ? ?",
+                choices: ["Sur le campus de Beaulieu", "Dans le centre-ville"],
+                answer: 1
+            },
+            {
+                title: "Quelle est la particularité de cette navette",
+                choices: ["elle fonctionne au gaz", "elle est entièrement autonome"],
+                answer: 1
+            },
+        ]
+    },
+    {
+        type: "map",
+        theme:"transport",
+        title: "Seriez-vous capable de relier les points qui forment la nouvelle ligne B du métro de Rennes ?"
+    }
+    
 ];
 
 var GAME = {
